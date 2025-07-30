9b7cc611e5702694c928ba737148eb92564195fa
S Shandu (21531591) <21531591@dut4life.ac.za>
2025-07-30 20:24:46 +0200

    p2

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,24 @@
+    // Modal behavior
+    const modal = document.getElementById("myModal");
+    const btn = document.getElementById("openModal");
+    const span = document.getElementById("closeModal");
+
+    btn.onclick = function (e) {
+      e.preventDefault();
+      modal.style.display = "block";
+    }
+
+    span.onclick = function () {
+      modal.style.display = "none";
+    }
+
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+	//-------------------------------------------------------------------------------
+
 (function($) {
 
 	$('.owl-carousel').owlCarousel({
